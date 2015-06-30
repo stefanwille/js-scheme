b3ba3782ad2f2c013d1993a1d8d56fdb3a10490f
Stefan Wille <github@stefanwille.com>
2015-06-30 13:26:04 +0200

    .

--- test/lexer_test.js
+++ test/lexer_test.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var assert = require('assert');
+var expect = require('chai').expect;
 
 function Symbol(name) {
   this.name = name;
 }
+
 
 function Token(token) {
   this.token = token;
@@ -23,83 +24,81 @@ describe('Lexer', function() {
   describe('#token()', function() {
     it('returns the current token', function() {
       var lexer = new Lexer('1');
-      assert.strictEqual(lexer.token(), 1);
+      expect(lexer.token()).to.equal(1);
     })
 
     it('can be called many times on the current token', function() {
       var lexer = new Lexer('1');
-      assert.strictEqual(lexer.token(), 1);
-      assert.strictEqual(lexer.token(), 1);
-      assert.strictEqual(lexer.token(), 1);
+      expect(lexer.token()).to.equal(1);
+      expect(lexer.token()).to.equal(1);
+      expect(lexer.token()).to.equal(1);
     })
   })
 
   describe('#next()', function() {
     it('returns true if there are more tokens to read', function() {
       var lexer = new Lexer('1');
-      assert.equal(lexer.next(), true);
+      expect(lexer.next()).to.be.true;
     })
 
     it('returns false if end of file', function() {
       var lexer = new Lexer('');
-      assert.equal(lexer.next(), false);
+      expect(lexer.next()).to.be.false;
     })
 
     it('advances the lexer to the next token', function() {
       var lexer = new Lexer('1 2 3');
-      assert.strictEqual(lexer.token(), 1);
-      assert.equal(lexer.next(), true);
-      assert.strictEqual(lexer.token(), 2);
-      assert.equal(lexer.next(), true);
-      assert.strictEqual(lexer.token(), 3);
-      assert.equal(lexer.next(), false);
+      expect(lexer.token()).to.equal(1);
+      expect(lexer.next()).to.be.true;
+      expect(lexer.token()).to.equal(2);
+      expect(lexer.next()).to.be.true;
+      expect(lexer.token()).to.equal(3);
+      expect(lexer.next()).to.be.false;
     })
 
     it('accepts numbers', function() {
       var lexer = new Lexer('12345');
-      assert.equal(lexer.token(), 12345);
+      expect(lexer.token()).to.equal(12345);
     })
 
     it('accepts strings', function() {
       var lexer = new Lexer(' "Hello" ');
-      assert.equal(lexer.token(), 'Hello');
+      expect(lexer.token()).to.equal('Hello');
     })
 
     it('accepts symbols', function() {
       var lexer = new Lexer('define + < ready?');
-      assert.equal(lexer.token(), new Symbol('define'));
+      expect(lexer.token()).to.equal(new Symbol('define'));
       lexer.next();
-      assert.equal(lexer.token(), new Symbol('+'));
+      expect(lexer.token()).to.equal(new Symbol('+'));
       lexer.next();
-      assert.equal(lexer.token(), new Symbol('<'));
+      expect(lexer.token()).to.equal(new Symbol('<'));
       lexer.next();
-      assert.equal(lexer.token(), new Symbol('ready?'));
-      lexer.next();
+      expect(lexer.token()).to.equal(new Symbol('ready?'));
     })
 
     it('accepts booleans', function() {
       var lexer = new Lexer('#t #f');
-      assert.equal(lexer.token(), true);
+      expect(lexer.token()).to.be.true;
       lexer.next();
-      assert.equal(lexer.token(), false);
+      expect(lexer.token()).to.be.false;
     })
 
     it('accepts parentheses', function() {
       var lexer = new Lexer('()');
-      assert.equal(lexer.token(), LEFT_PARENTHESIS);
+      expect(lexer.token()).to.equal(LEFT_PARENTHESIS);
       lexer.next();
-      assert.equal(lexer.token(), RIGHT_PARENTHESIS);
+      expect(lexer.token()).to.equal(RIGHT_PARENTHESIS);
     })
 
     it('accepts quoted parentheses', function() {
       var lexer = new Lexer("'()");
-      assert.equal(lexer.token(), "'");
+      expect(lexer.token()).to.equal("'");
       lexer.next();
-      assert.equal(lexer.token(), LEFT_PARENTHESIS);
+      expect(lexer.token()).to.equal(LEFT_PARENTHESIS);
       lexer.next();
-      assert.equal(lexer.token(), RIGHT_PARENTHESIS);
+      expect(lexer.token()).to.equal(RIGHT_PARENTHESIS);
     })
   })
 })
-
 
