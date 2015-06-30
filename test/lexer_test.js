eb7a124c556a045b9ffdc1760ea04b64a4c8b1a6
Stefan Wille <github@stefanwille.com>
2015-06-30 12:04:13 +0200

    .

--- test/lexer_test.js
+++ test/lexer_test.js
@@ -2,37 +2,35 @@
 
 var assert = require('assert');
 
+function Symbol(name) {
+  this.name = name;
+}
+
+function Token(token) {
+  this.token = token;
+}
+
+var LEFT_PARENTHESIS = new Token('(');
+var RIGHT_PARENTHESIS = new Token(')');
+
 function Lexer(input) {
   this.next = function() { return true; };
-  this.token = function() { return 'NUMBER'; };
-  this.value = function() { return 1; };
+  this.token = function() { return 1; };
 }
+
 
 describe('Lexer', function() {
   describe('#token()', function() {
-    it('returns the current tokens type', function() {
+    it('returns the current token', function() {
       var lexer = new Lexer('1');
-      assert.equal(lexer.token(), 'NUMBER');
+      assert.strictEqual(lexer.token(), 1);
     })
 
     it('can be called many times on the current token', function() {
       var lexer = new Lexer('1');
-      assert.equal(lexer.token(), 'NUMBER');
-      assert.equal(lexer.token(), 'NUMBER');
-      assert.equal(lexer.token(), 'NUMBER');
-    })
-  })
-
-  describe('#value()', function() {
-    it('returns the current tokens value', function() {
-      var lexer = new Lexer('1')
-      assert.strictEqual(1, lexer.value())
-    })
-
-    it('can be called many times on the current token', function() {
-      var lexer = new Lexer('1');
-      assert.equal(lexer.value(), 1);
-      assert.equal(lexer.token(), 1);
+      assert.strictEqual(lexer.token(), 1);
+      assert.strictEqual(lexer.token(), 1);
+      assert.strictEqual(lexer.token(), 1);
     })
   })
 
@@ -49,54 +47,48 @@ describe('Lexer', function() {
 
     it('advances the lexer to the next token', function() {
       var lexer = new Lexer('1 2 3');
-      assert.equal(lexer.token(), 'NUMBER');
+      assert.strictEqual(lexer.token(), 1);
       assert.equal(lexer.next(), true);
-      assert.equal(lexer.token(), 'NUMBER');
+      assert.strictEqual(lexer.token(), 2);
       assert.equal(lexer.next(), true);
-      assert.equal(lexer.token(), 'NUMBER');
+      assert.strictEqual(lexer.token(), 3);
       assert.equal(lexer.next(), false);
     })
 
     it('accepts numbers', function() {
       var lexer = new Lexer('12345');
-      assert.equal(lexer.token(), 'NUMBER');
+      assert.equal(lexer.token(), 12345);
     })
 
     it('accepts strings', function() {
       var lexer = new Lexer(' "Hello" ');
-      assert.equal(lexer.token(), 'STRING');
+      assert.equal(lexer.token(), 'Hello');
     })
 
     it('accepts symbols', function() {
       var lexer = new Lexer('define + < ready?');
-      assert.equal(lexer.token(), 'SYMBOL');
-      assert.equal(lexer.value(), 'define');
+      assert.equal(lexer.token(), new Symbol('define'));
       lexer.next();
-      assert.equal(lexer.token(), 'SYMBOL');
-      assert.equal(lexer.value(), '+');
+      assert.equal(lexer.token(), new Symbol('+'));
       lexer.next();
-      assert.equal(lexer.token(), 'SYMBOL');
-      assert.equal(lexer.value(), '<');
+      assert.equal(lexer.token(), new Symbol('<'));
       lexer.next();
-      assert.equal(lexer.token(), 'SYMBOL');
-      assert.equal(lexer.value(), 'ready?');
+      assert.equal(lexer.token(), new Symbol('ready?'));
       lexer.next();
     })
 
     it('accepts booleans', function() {
       var lexer = new Lexer('#t #f');
-      assert.equal(lexer.token(), 'BOOLEAN');
-      assert.equal(lexer.value(), true);
+      assert.equal(lexer.token(), true);
       lexer.next();
-      assert.equal(lexer.token(), 'BOOLEAN');
-      assert.equal(lexer.value(), false);
+      assert.equal(lexer.token(), false);
     })
 
     it('accepts parentheses', function() {
       var lexer = new Lexer('()');
-      assert.equal(lexer.token(), "(");
+      assert.equal(lexer.token(), LEFT_PARENTHESIS);
       lexer.next();
-      assert.equal(lexer.token(), ")");
+      assert.equal(lexer.token(), RIGHT_PARENTHESIS);
     })
   })
 })
